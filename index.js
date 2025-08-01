fa1b7c67b5124fb36c7801a32963918a4f133895
「安忠琪」 <1695603363@qq.com>
2025-08-01 17:45:27 +0800

    Refactor map layers and update terrain and icons
    Switched map center and zoom to focus on a new region, adjusted MVT minzoom, and improved label rendering with dynamic text sizes. Updated the air-line label to use a new custom icon and simplified its text field. Removed OSM and old terrain sources in favor of mapbox-terrain-dem, and updated style and sprite assets accordingly. Added new sprite and icon images, and commented out unused administrative and terrain layers.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const MAP_CONTAINER_ID = "map";
-const MAP_CENTER = [-122.447303, 37.753574];
-const MAP_ZOOM = 2;
-// const MAP_CENTER = [11.39085, 47.27574];
-// const MAP_ZOOM = 12;
+// const MAP_CENTER = [-122.447303, 37.753574];
+// const MAP_ZOOM = 2;
+const MAP_CENTER = [11.39085, 47.27574];
+const MAP_ZOOM = 12;
 
 const MVT_SOURCE_ID = "contours8989";
 const TILE_KEY = "get_your_own_OpIi9ZULNHzrESv6T2vL";
@@ -10,7 +10,7 @@ const MVT_TILES = [
     "https://aips.siniswift.com/gis//spaces/features/NATURE/{z}_{x}_{y}.mvt",
     "https://aips.siniswift.com/gis//spaces/features/BASE/{z}_{x}_{y}.mvt",
 ];
-const MVT_MINZOOM = 0;
+const MVT_MINZOOM = 1;
 const MVT_MAXZOOM = 15;
 
 window.addEventListener("DOMContentLoaded", () => {
@@ -72,7 +72,21 @@ function addMVT(map) {
                 layout: {
                     "symbol-placement": "line",
                     "text-field": ["get", "codeId"],
-                    "text-size": 12,
+                    "text-size": [
+                        "interpolate",
+                        [
+                            "linear"
+                        ],
+                        [
+                            "zoom"
+                        ],
+                        1,
+                        8,
+                        2,
+                        9,
+                        3,
+                        11, 4, 12, 5, 13
+                    ],
                     "text-offset": [0, 1],
                     "text-optional": true
                 },
@@ -92,67 +106,104 @@ function addMVT(map) {
                 },
             });
             // 航线名称
+            // map.addLayer({
+            //     id: "line-label",
+            //     type: "symbol",
+            //     source: "baseMvt", // 对应的 vector source
+            //     "source-layer": "segment_202507", // 线图层名
+            //     layout: {
+            //         "symbol-placement": "line", // 关键属性，表示文字沿线分布
+            //         "text-size": 12,
+            //         "text-field": [
+            //             "format",
+            //             ["get", "txtDesigHigh"],
+            //             {
+            //                 "font-scale": 0.8,
+            //                 "text-color": "#536577",
+            //                 "text-halo-width": 0,
+            //             },
+            //         ], // 或固定文本，比如 "路线A"
+            //         "text-letter-spacing": 0.05,
+            //         "text-offset": [0, 2],
+            //     },
+            //     paint: {
+            //     },
+            //     minzoom: 6,
+            // });
+            // map.addLayer({
+            //     id: "line-label-1",
+            //     type: "symbol",
+            //     source: "baseMvt", // 对应的 vector source
+            //     "source-layer": "segment_202507", // 线图层名
+            //     layout: {
+            //         "symbol-placement": "line", // 关键属性，表示文字沿线分布
+            //         "text-size": 12,
+            //         "text-field": [
+            //             "format",
+            //             ["get", "valLen"],
+            //             {
+            //                 "font-scale": 0.8,
+            //                 "text-color": "#536577",
+            //                 "text-border-width": "3",
+            //                 "text-halo-width": 0,
+            //             },
+            //             ["get", "uomDist"],
+            //             {
+            //                 "font-scale": 0.8,
+            //                 "text-color": "#536577",
+            //                 "text-halo-width": 0,
+            //             },
+            //             "\n",
+            //             ["get", "txtDesig"],
+            //             {
+            //                 "font-scale": 1,
+            //                 "text-color": "#fff",
+            //                 "text-halo-width": 0,
+            //             },
+            //             "\n",
+            //             ["get", "txtDesig"],
+            //             {
+            //                 "font-scale": 0.8,
+            //                 "text-color": "#536577",
+            //                 "text-halo-width": 0,
+            //             }
+            //         ], // 或固定文本，比如 "路线A"
+            //         "text-letter-spacing": 0.05,
+            //         "icon-image": "text-bg-t",
+            //         "icon-offset": [3, 10],
+            //         "icon-text-fit": "both",
+            //         "icon-text-fit-padding": [1, 8, 1, 8],
+            //     },
+            //     paint: {
+            //     },
+            //     minzoom: 6,
+            // });
             map.addLayer({
-                id: "line-label",
+                id: "air-line-label",
                 type: "symbol",
                 source: "baseMvt", // 对应的 vector source
                 "source-layer": "segment_202507", // 线图层名
+                // 过滤出txtDesig不为空的数据和不为null的数据
+                filter: [
+                    "all",
+                    ["!=", ["get", "txtDesig"], ""],
+                    ["!=", ["get", "txtDesig"], null],
+                    ["!=", ["get", "txtDesig"], " "]
+                ],
                 layout: {
+                    "icon-image": "custom-icon",
+                    // 图片偏移
+                    "icon-offset": [3, 0],
                     "symbol-placement": "line", // 关键属性，表示文字沿线分布
-                    "text-size": 14,
-                    // "icon-image": 'border',
-                    // "symbol-spacing": [
-                    //   "interpolate",
-                    //   ["linear"],
-                    //   ["zoom"],
-                    //   11,
-                    //   400,
-                    //   14,
-                    //   600,
-                    //   16,
-                    //   800,
-                    //   22,
-                    //   1200,
-                    // ],
-                    // "symbol-placement": ["step", ["zoom"], "point", 11, "line"],
-                    // "text-rotation-alignment": "viewport",
+                    "text-size": 12,
                     "icon-text-fit": "both",
-                    "icon-text-fit-padding": [2, 5, 2, 5],
-
-                    "text-field": [
-                        "format",
-                        ["get", "valLen"],
-                        {
-                            "font-scale": 0.8,
-                            "text-color": "#536577",
-                            "text-border-width": "3",
-                            "text-halo-width": 0,
-                        },
-                        ["get", "uomDist"],
-                        {
-                            "font-scale": 0.8,
-                            "text-color": "#536577",
-                            "text-halo-width": 0,
-                        },
-                        "\n",
-                        ["get", "txtDesig"],
-                        {
-                            "font-scale": 1,
-                            "text-halo-width": 1,
-                            "text-halo-blur": 0.7,
-                            "text-halo-color": "#ffffff",
-                        },
-                        "\n",
-                        ["get", "txtDesigHigh"],
-                        {
-                            "font-scale": 0.8,
-                            "text-color": "#536577",
-                            "text-halo-width": 0,
-                        },
-                    ], // 或固定文本，比如 "路线A"
+                    "icon-text-fit-padding": [1, 8, 1, 8],
+                    "text-field": ["get", "txtDesig"], // 或固定文本，比如 "路线A"
                     "text-letter-spacing": 0.05,
                 },
-                paint: {},
+                paint: {
+                    "text-color": "#fff",
+                },
                 minzoom: 6,
             });
             // 航路点
@@ -215,19 +266,19 @@ function addMVT(map) {
                 },
             });
             // administrative 图层 行政区域
-            map.addLayer(
-                {
-                    id: "administrativeLayer",
-                    type: "fill",
-                    source: "maplibreCountries",
-                    "source-layer": "land",
-                    paint: {
-                        "fill-color": "#fff",
-                        "fill-opacity": 1,
-                    },
-                },
-                "oceanLayer"
-            );
+            // map.addLayer(
+            //     {
+            //         id: "administrativeLayer",
+            //         type: "fill",
+            //         source: "maplibreCountries",
+            //         "source-layer": "land",
+            //         paint: {
+            //             "fill-color": "#fff",
+            //             "fill-opacity": 1,
+            //         },
+            //     },
+            //     "oceanLayer"
+            // );
 
             // 添加机场轮廓
             // map.addLayer({
@@ -287,25 +338,16 @@ function setupLayerControlPanel(map) {
                 }
             } else if (layer.id === 'terrain') {
                 if (this.checked) {
-                    map.addLayer({
-                        id: 'hills',
-                        type: 'hillshade',
-                        source: 'hillshadeSource',
-                        layout: { visibility: 'visible' },
-                        paint: { 'hillshade-shadow-color': '#473B24' },
-
-                    }, "oceanLayer")
-                    map.addLayer({
-                        id: 'osm',
-                        type: 'raster',
-                        source: 'osm'
-                    }, "oceanLayer")
-                    map.setTerrain({ 'source': 'terrainSource', 'exaggeration': 1 }); // 开启地形
+                    // map.addLayer({
+                    //     "id": "mapbox-terrain-dem-layer",
+                    //     "source": "mapbox-terrain-dem",
+                    //     "type": "raster"
+                    // }, "oceanLayer")
+                    map.setTerrain({ 'source': 'mapbox-terrain-dem', 'exaggeration': 300 }); // 开启地形
                     map.addControl(new maplibregl.TerrainControl({
-                        source: 'terrainSource',
+                        source: 'mapbox-terrain-dem',
                         exaggeration: 1
                     }));
-                    map.removeLayer('administrativeLayer')
                 } else { }
             } else {
                 const visibility = this.checked ? "visible" : "none";
